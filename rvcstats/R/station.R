d4f26e4b8889a8507e2084f7e328891099c9b13f
Harry Ganz <hganz@rsmas.miami.edu>
2014-09-19 18:28:35 -0400

    Added all fields to station initializer

--- rvcstats/R/station.R
+++ rvcstats/R/station.R
@@ -1,4 +1,4 @@
-## Class: With fields for every sampling station
+## Class: With fields for every secondary sampling station
 Station = setRefClass("Station",
                      fields = list(num = "numeric", len = "numeric", species_nr = "numeric",
                      time_seen = "numeric", station_nr = "numeric", depth = "numeric",
@@ -7,8 +7,11 @@ Station = setRefClass("Station",
 Station$accessors(c("num", "len", "species_nr", "time_seen", "station_nr",
                     "depth", "underwater_visibility"))
 Station$methods(
-  ## Init: Initializes required fields, other fields are initialized as empty
-  initialize = function(num, len, species_nr, station_nr, time_seen){
-    setNum(num); setLen(len); setSpecies_nr(species_nr); setTime_seen(time_seen)
+  ## Init: Initializes required fields, other fields are initialized as NULL
+  initialize = function(num, len, species_nr, station_nr, time_seen, depth = numeric(0), 
+                        underwater_visibility = numeric(0)){
+    setNum(num); setLen(len); setSpecies_nr(species_nr); setTime_seen(time_seen);
+    setStation_nr(station_nr); setDepth(depth);
+    setUnderwater_visibility(underwater_visibility)
   }
   )
