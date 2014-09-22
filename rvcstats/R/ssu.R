6ba098684954024e87c1c958693d2bb16ddb60ca
Harry Ganz <hganz@rsmas.miami.edu>
2014-09-22 14:13:08 -0400

    Modified ssu.R to no longer include density

--- rvcstats/R/ssu.R
+++ rvcstats/R/ssu.R
@@ -1,20 +1,18 @@
 ## Class: With fields for every secondary sampling unit
 SSU = setRefClass("SSU",
-                     fields = list(num = "numeric", len = "numeric", species_nr = "numeric",
-                     time_seen = "numeric", station_nr = "numeric", depth = "numeric",
-                     underwater_visibility = "numeric", area = "numeric", 
-                     density = "numeric"))
+                     fields = list(num = "numeric", len = "numeric", species_cd = "factor", species_nr = "numeric",
+                     time_seen = "factor", station_nr = "numeric", depth = "numeric",
+                     underwater_visibility = "numeric", area = "numeric"))
 ## Returns: Getters and setters for every field listed
 SSU$accessors(c("num", "len", "species_nr", "time_seen", "station_nr",
                     "depth", "underwater_visibility", "area", "density"))
 SSU$methods(
   ## Init: Initializes required fields, other fields are initialized as numeric vector
   ## of length zero
-  initialize = function(num, len, species_nr, station_nr, time_seen, depth = numeric(0), 
+  initialize = function(num, len, species_nr, species_cd, station_nr, time_seen, depth = numeric(0), 
                         underwater_visibility = numeric(0), area = 177){
     setNum(num); setLen(len); setSpecies_nr(species_nr); setTime_seen(time_seen);
     setStation_nr(station_nr); setDepth(depth);setArea(area)
     setUnderwater_visibility(underwater_visibility);
-    density <<- num/area
   }
   )
