826839fc0130925c313141779bf27b4bb38761a5
Harry Ganz <hganz@rsmas.miami.edu>
2014-09-21 20:50:15 -0400

    Added calculated density field to ssu.R

--- rvcstats/R/ssu.R
+++ rvcstats/R/ssu.R
@@ -2,17 +2,19 @@
 SSU = setRefClass("SSU",
                      fields = list(num = "numeric", len = "numeric", species_nr = "numeric",
                      time_seen = "numeric", station_nr = "numeric", depth = "numeric",
-                     underwater_visibility = "numeric", area = "numeric"))
+                     underwater_visibility = "numeric", area = "numeric", 
+                     density = "numeric"))
 ## Returns: Getters and setters for every field listed
 SSU$accessors(c("num", "len", "species_nr", "time_seen", "station_nr",
-                    "depth", "underwater_visibility", "area"))
+                    "depth", "underwater_visibility", "area", "density"))
 SSU$methods(
   ## Init: Initializes required fields, other fields are initialized as numeric vector
   ## of length zero
   initialize = function(num, len, species_nr, station_nr, time_seen, depth = numeric(0), 
                         underwater_visibility = numeric(0), area = 177){
     setNum(num); setLen(len); setSpecies_nr(species_nr); setTime_seen(time_seen);
-    setStation_nr(station_nr); setDepth(depth);
-    setUnderwater_visibility(underwater_visibility)
+    setStation_nr(station_nr); setDepth(depth);setArea(area)
+    setUnderwater_visibility(underwater_visibility);
+    density <<- num/area
   }
   )
