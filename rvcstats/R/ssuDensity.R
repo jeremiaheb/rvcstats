bd104e8c0b4a9166091c2a3c393e9f8b2357ca14
Harry Ganz <hganz@rsmas.miami.edu>
2014-09-24 14:30:12 -0400

    Made ssuDensity return data.frame instead of RVC

--- rvcstats/R/ssuDensity.R
+++ rvcstats/R/ssuDensity.R
@@ -1,8 +1,11 @@
-## Returns: an RVC object with densities for each seconday sample unit added
+## Returns: a data.frame with densities for each seconday sample unit added
 ssuDensity = function(rvcObj, ssu.area = 177){
   if (!inherits(rvcObj, "RVC")){
     stop("rvcObj must be of class RVC")
   }
+  ## Calculate Densities
   rvcObj$DENS = rvcObj$NUM/ssu.area
-  return(rvcObj)
+  ## Reclasiify so it can be coerced to data.frame
+  class(rvcObj) = "list"
+  return(as.data.frame(rvcObj))
 }
