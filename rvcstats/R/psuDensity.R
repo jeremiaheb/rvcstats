4fcddb1c13749e928c3646f997423f119dc6cd02
Harry Ganz <hganz@rsmas.miami.edu>
2014-09-29 14:19:45 -0400

    Renamed psuAbund to psuDensity and changed output variable name Changed avg.abun variable to dbar

--- rvcstats/R/psuDensity.R
+++ rvcstats/R/psuDensity.R
@@ -1,6 +1,7 @@
-## Returns: the avg. counts, variance in counts, number of SSUs, and replicate status 
-## (np.freq) for each PSU, given an RVC object
-psuAbund = function(rvcObj){
+## Returns: the avg. counts (dbar), variance in counts (vari), 
+## number of SSUs (m), and replicate status (np.freq) for each PSU.
+## Given an RVC object
+psuDensity = function(rvcObj){
   ## Check type
   if (!inherits(rvcObj, "RVC")){
     stop("rvcObj must be of class RVC. Type ?rvcData for more information")
@@ -9,16 +10,20 @@ psuAbund = function(rvcObj){
   class(rvcObj) = "list"
   ## Set the variables by which to aggregate
   agg.by = rvcObj[names(rvcObj) %w/o% c("STATION_NR", "NUM")]
-  ## Avg. counts by PSU
+  
+  ## Avg. counts in SSU by PSU
   psu = aggregate(rvcObj$NUM, by = agg.by, FUN = mean)
-  names(psu)[length(names(psu))] = "avg.abun"
-  ## Variance in counts by PSU
+  names(psu)[length(names(psu))] = "dbar"
+  
+  ## Variance in counts of each SSU by PSU
   psu$vari = aggregate(rvcObj$NUM, by = agg.by,
                       FUN = function(x){
                         ifelse(is.na(var(x)),0,var(x))
                       })$x
+  
   ## Number of SSUs per PSU
   psu$m = aggregate(rvcObj$NUM, by = agg.by, FUN = length)$x
+  
   ## Replicate status
   psu$np.freq = ifelse(psu$m>1,1,0)
   
