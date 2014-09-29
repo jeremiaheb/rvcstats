248fea9bb562e3e9d21cce08277ac9113029ad0f
Harry Ganz <hganz@rsmas.miami.edu>
2014-09-29 10:10:01 -0400

    Changed stratAbund method so it includes overall stratum sample variance Changed the parameters to include psu and ssu area so Mtot could be calculated, also chnaged output data.frame to include overall stratum variance

--- rvcstats/R/stratAbund.R
+++ rvcstats/R/stratAbund.R
@@ -1,7 +1,9 @@
 ## Returns: a data.frame including the average count (avg.abund), avg. number of 
-## SSUs (m), number of PSUs (n), variance in PSU abundance (v1) and variance 
-## in SSU abundance (v2) for each stratum in a given RVC object
-stratAbund = function(rvcObj) {
+## SSUs (avg.m), number of PSUs (n), variance in PSU abundance (v1) and variance 
+## in SSU abundance (v2) and total sample variance vari for each stratum in a given 
+## RVC object, Given an RVC object, and the ssu.area (default=177m^2) and the psu.area
+## (default=40000m^2)
+stratAbund = function(rvcObj, ssu.area = 177, psu.area = 40000) {
   ## Calculate mean abundances for PSUs 
   psu = psuAbund(rvcObj)
   ## Calculate average counts and average m per stratum
@@ -20,8 +22,9 @@ stratAbund = function(rvcObj) {
   xx = aggregate(cbind(vari, m, np.freq) ~ SPECIES_CD + YEAR + STRAT, data = psu,
                       FUN = sum)
   strat$v2 = ifelse(xx$np>0, xx$vari/xx$np, 0)
+  ## Calculate overall stratum variance
+  strat$vari = strat$v1 - strat$v2/(psu.area/ssu.area)
   strat$nm = xx$m
   rm(xx)
-  
 return(strat)
 }
