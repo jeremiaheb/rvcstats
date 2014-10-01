9715dd38969d4f3d301cf6da7527a3cf8f0b2773
Harry Ganz <hganz@rsmas.miami.edu>
2014-10-01 14:40:33 -0400

    Added helper function to parse full scientific names into species codes

---
+++ rvcstats/R/toSpcCd.R
@@ -0,0 +1,10 @@
+## Helper Function to create a species code from a scientific name
+.toSpcCd = function(s){
+  x = sapply(s, FUN = function(s){
+    spl = strsplit(s, " ")
+    f = substring(spl[[1]][1],1,3)
+    l = substring(spl[[1]][2],1,4)
+    return(toupper(paste(f,l,sep = " ")))
+  })
+  return(as.vector(x))
+}
