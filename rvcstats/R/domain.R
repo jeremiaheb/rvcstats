51224ee10ef0e0aa3b74c0101e815271b2612eef
Harry Ganz <hganz@rsmas.miami.edu>
2014-10-21 11:09:44 -0400

    Renamed domainDensity to domain and modified it to calculate occurrence as well as density

--- rvcstats/R/domain.R
+++ rvcstats/R/domain.R
@@ -1,9 +1,9 @@
-## Returns: The average density per SSU (177m^2), variance, SE, and CV
+## Returns: The average density/occurence (yi) per SSU (177m^2), variance, SE, and CV
 ## and total number of samples nm for the entire sampling domain
 ## Given: An RVC object, a STRAT object, and optional parameters passed
 ## to stratDensity
-domainDensity = function(rvcObj, stratObj, ...){
-  strat = stratDensity(rvcObj, stratObj, ...)
+domain = function(rvcObj, stratObj, ...){
+  strat = strat(rvcObj, stratObj, ...)
   ## If strat density includes protected areas, reweight by protected status
   if ("PROT" %in% names(strat)){
     xx = aggregate(NTOT ~ YEAR + PROT, data = unique(strat[c("NTOT","YEAR","PROT")]), FUN = sum)
@@ -12,17 +12,18 @@ domainDensity = function(rvcObj, stratObj, ...){
     xx = xx[order(xx$id),] ## Retrieve order of of strat
     strat$wh = xx$NTOT.x/xx$NTOT.y
     ## Clean Up
+    rm(xx)
     strat = strat[names(strat) %w/o% "id"]
   }
   ## Select aggregate by variables
-  agg.by = as.list(strat[names(strat) %w/o% c("NTOT","STRAT", "wh", "dbar", "mbar", "n",
+  agg.by = as.list(strat[names(strat) %w/o% c("NTOT","STRAT", "wh", "yi", "mbar", "n",
                                       "v1", "v2", "nm", "v2", "vbar", "NMTOT")])
   ## Calculate density, variance, SE, CV, and nm
-  domain = with(strat, aggregate(wh*dbar, by = agg.by, FUN = sum))
-  names(domain)[length(names(domain))] = "density"
+  domain = with(strat, aggregate(wh*yi, by = agg.by, FUN = sum))
+  names(domain)[length(names(domain))] = "yi"
   domain$variance = with(strat, aggregate(wh^2*vbar, by = agg.by, FUN = sum)$x)
   domain$se = sqrt(domain$variance)
-  domain$cv = with(domain, (se/density)*100)
+  domain$cv = with(domain, (se/yi)*100)
   domain$nm = with(strat, aggregate(nm, by = agg.by, FUN = sum)$x)
   
   return(domain)
