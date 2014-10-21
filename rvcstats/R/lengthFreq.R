9cd65f8be6679f0d155930d02b7cec6773fe440b
Harry Ganz <hganz@rsmas.miami.edu>
2014-10-21 15:23:23 -0400

    Modified length frequency to be a helper function within rvcData

--- rvcstats/R/lengthFreq.R
+++ rvcstats/R/lengthFreq.R
@@ -1,30 +1,17 @@
 ## Returns: The length frequency for each stratum for a given species and 
 ## RVC data, years, and strata
-.lengthFreq = function(data, species, years = "all", strata = "all", includes.protected){
-  ## Make sure data names are uppercase
-  names(data) = toupper(names(data))
-  ## subset by years in years
-  if (years != "all") {
-    data = subset(data, YEAR %in% years)
-  }
-  ## Subset by strata in strata
-  if (strata != "all") {
-    data = subset(data, STRAT %in% strata)
-  }
-  ## Subset by species in species
-  data = subset(data, SPECIES_CD %in% species)
+.lengthFreq = function(data, includes.protected){
   
   ## Set agg.by variables
   agg.by = c("SPECIES_CD", "YEAR", "STRAT", "LEN")
   
   ## If includes.protected is TRUE aggregate by that as well
   if (includes.protected){
-    data$PROT = ifelse(data$MPA_NR > 0, 1,0)
     agg.by = c("PROT", agg.by)
   }
+  
   ## Total number counted per length class
   lenf = aggregate(data$NUM, by = as.list(data[agg.by]), FUN = sum)
-  
   names(lenf)[length(names(lenf))] = "COUNT"
   
   agg.by = agg.by[-length(agg.by)] # Change agg.by to not include length
