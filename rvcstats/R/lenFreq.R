faba466d33c0e202e5185bc3f20d9b25768f2bbe
Harry Ganz <hganz@rsmas.miami.edu>
2014-10-22 15:31:12 -0400

    Modified lenFreq to calculate length frequency aggregated by agg.by parameter

--- rvcstats/R/lenFreq.R
+++ rvcstats/R/lenFreq.R
@@ -1,10 +1,12 @@
-## Returns: Length Frequency information aggregated by species, year,
-## stratum and, if includes.protected = TRUE, protected status
-lenf = function(data, species, years = "all", strata = "all", 
-                includes.protected = FALSE){
+## Returns: Length Frequency data aggregated by species 
+## and agg.by variables (default = "YEAR"), 
+## Possible agg.by variables include "YEAR", "STRAT", and "PROT"
+lenFreq = function(data, species, years = "all", strata = "all", 
+                agg.by = c("YEAR")){
   ## Check to make sure variable names are correct in data
   names(data) = toupper(names(data))
-  reqd = c("SPECIES_CD", "YEAR", "STRAT", "PRIMARY_SAMPLE_UNIT", "STATION_NR", "NUM", "LEN")
+  reqd = c("SPECIES_CD", "YEAR", "STRAT", "PRIMARY_SAMPLE_UNIT",
+           "STATION_NR", "NUM", "LEN")
   .inList("required variables", reqd, names(data))
   
   ## If years != all, subset by selected years
@@ -20,4 +22,39 @@ lenf = function(data, species, years = "all", strata = "all",
     ## check that selected strata are in data
     .inList("strata", strata, unique(data$STRAT))
   }
+  ## If PROT is an agg.by variable, code for PROT
+  if ("PROT" %in% agg.by){
+    ## Check that MPA_NR is available
+    .inList("required variable", "MPA_NR", names(data))
+    data$PROT = ifelse(data$MPA_NR > 0, 1, 0)
+  }
+  ## Set species to species codes
+  species = .toSpcCd(species)
+  ## Subset by species
+  data = subset(data, SPECIES_CD %in% species)
+  
+  ## Add species to grouping
+  agg.by = c("SPECIES_CD", agg.by)
+  
+  ## Total for each grouping
+  xx = aggregate(data$NUM, by = as.list(data[agg.by]), FUN = sum)
+  names(xx)[length(names(xx))] = "TOTAL"
+  
+  ## Add LEN to agg.by variables
+  agg.by = c(agg.by, "LEN")
+  
+  ## Total for each length class within each grouping
+  yy = aggregate(data$NUM, by = as.list(data[agg.by]), FUN = sum)
+  names(yy)[length(names(yy))] = "COUNT"
+  
+  ## Merge by original agg.by variables
+  agg.by = agg.by[-length(agg.by)]
+  zz = merge(xx,yy,by = agg.by)
+  rm(xx,yy)
+  
+  ## Calculate and return length frequencies
+  zz$frequency = zz$COUNT/zz$TOTAL
+  zz = zz[names(zz) %w/o% c("COUNT", "TOTAL")]
+  return(zz)
+  
 }
