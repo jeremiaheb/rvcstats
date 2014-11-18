05e4a8bcb9ed0817034d15b9ac860a6ed807d5b0
Harry Ganz <hganz@rsmas.miami.edu>
2014-11-18 09:22:56 -0500

    Modified strat to reflect changes to .psu
    No longer takes a psu and ssu size argument. Takes only an rvcObj and calc argument

--- rvcstats/R/strat.R
+++ rvcstats/R/strat.R
@@ -3,95 +3,48 @@
 ## avg. SSUs per PSU (mbar), number of PSUs (n), number of SSUs (nm),
 ## total number of possible PSUs (NTOT), and total number of possible
 ## SSUs (NMTOT) per Stratum
-## Given an RVC object, a STRAT obj, and SSU and PSU areas
-## 177m^2 and 40000m^2, by default
-strat = function(rvcObj, stratObj, calculate.density = TRUE, ssu.area = 177, psu.area = 40000) {
-  ## Make sure stratObj is of class STRAT
-  if (!inherits(stratObj, "STRAT")){
-    stop("stratObj must be of class STRAT, type ?stratData for more info")
-  }
-  ## TODO: Add wrapper so these checks becomes unneccessary
-  ## Check to make sure both include or exclude protected status
-  if (!is.null(rvcObj$PROT) & is.null(stratObj$PROT) || 
-        is.null(rvcObj$PROT) & !is.null(stratObj$PROT)){
-    stop("rvcObj and stratObj must have matching arguments for includes.protected")
-  }
-  ## Make sure years and strata in rvcObj and stratObj match
-  rvcYrs =  unique(rvcObj$YEAR)
-  strYrs = unique(stratObj$YEAR)
-  d = setdiff(rvcYrs, strYrs)
-  if (length(d)>0){
-    diff = paste(d, collapse = ", ")
-    stop(paste("years:", diff, " in rvcObj not found in stratObj"))
-  }
-  ## ToDo: Refactor this so it uses an apply function rather than a loop
-  ## Also should continue going to catch all exceptions, rather than just the first 
-  ## set
-  for (i in 1:length(rvcYrs)){
-    rvcStr = unique(rvcObj$STRAT[rvcObj$YEAR == rvcYrs[i]])
-    strStr = unique(stratObj$STRAT[stratObj$YEAR == rvcYrs[i]])
-    d = setdiff(rvcStr, strStr)
-    if (length(d)>0){
-      diff = paste(d, collapse = ", ")
-      stop(paste("strata:", diff, " in year ",
-                 rvcYrs[i], " in rvcObj not found in stratObj"))
-    }
-  }
-  
+## Given an RVC object and which parameter to calculate
+## 'd' for density (default) and 'p' for occurrence
+strat <- function(rvcObj, calc = "d") {
   ## Calculate PSU densities/occurrences
-  psu = .psu(rvcObj, calculate.density)
-  
+  psu <- .psu(rvcObj, calc);
   ## Set the variables by which to aggregate
-  agg.by = psu[names(psu) %w/o% c("PRIMARY_SAMPLE_UNIT", "NUM",
-                                  "yi", "vari", "m", "np.freq")]
+  agg_by <- c("SPECIES_CD", "YEAR", "STRAT");
+  ## If includes_protected is TRUE add to
+  ## aggregate by variables
+  if (attr(rvcObj, "includes_protected")){
+    agg_by  <- c(agg_by, "PROT");
+  }
+  ## Make agg_by into a list
+  agg_by  <- as.list(psu[agg_by]);
   ## Calculate average density/occurrence
-  strat = aggregate(psu$yi, by = agg.by, FUN = mean)
-  names(strat)[length(names(strat))] = "yi"
+  samp <- aggregate(list(yi = psu$yi), by = agg_by, FUN = mean);
   ## Calculate mbar
-  strat$mbar = aggregate(psu$m, by = agg.by, FUN = mean)$x
+  samp$mbar <- aggregate(psu$m, by = agg_by, FUN = mean)$x;
   ## Calculate n
-  strat$n = aggregate(psu$m, by = agg.by, FUN = length)$x
+  samp$n <- aggregate(psu$m, by = agg_by, FUN = length)$x;
   ## Calculate v1
-  strat$v1 = aggregate(psu$yi, by = agg.by, FUN = var)$x
+  samp$v1 <- aggregate(psu$yi, by = agg_by, FUN = var)$x;
   ## Calculate nm, v2, and np
-  strat$nm = aggregate(psu$m, by = agg.by, FUN = sum)$x
-  np = aggregate(psu$np.freq, by = agg.by, FUN = sum)$x
-  v2 = aggregate(psu$vari, by = agg.by, FUN = sum)$x
-  strat$v2 = ifelse(np>0, v2/np, 0)
+  samp$nm <- aggregate(psu$m, by = agg_by, FUN = sum)$x;
+  np <- aggregate(psu$np.freq, by = agg_by, FUN = sum)$x;
+  v2 <- aggregate(psu$vari, by = agg_by, FUN = sum)$x;
+  samp$v2 <- ifelse(np>0, v2/np, 0);
   rm(v2)
   
-  ## Cast stratObj to list 
-  class(stratObj) = "list"
-  ## Merge stratObj and strat
-  strat2 = merge(stratObj, strat)
+  ## Get stratum data
+  strat  <- rvcObj$stratum_data;
+  
+  ## Merge strat and samp
+  strat2 <- merge(strat, samp)
   
   ## Calculate MTOT
-  MTOT = round(psu.area/ssu.area, 0)
+  MTOT <- with(strat,round(GRID_SIZE^2/(pi*7.5^2), 0));
   ## Calculate variance weights, variance, and NMTOT
-  fn = strat2$n/strat2$NTOT
-  fm = strat2$mbar/MTOT
-  strat2$vbar = with(strat2, ((1-fn)*v1/n)+((fn*(1-fm)*v2)/nm))
-  strat2$NMTOT = strat2$NTOT*MTOT
-  
-  ## If strat2 includes protected areas, weight strata by year and protected status,
-  ## otherwise weight only by year
-  if ("PROT" %in% names(strat2)){
-    dat = unique(strat2[c("NTOT","YEAR","PROT")])
-    f = formula(NTOT ~ YEAR + PROT)
-    by = c("YEAR", "PROT")
-  } else {
-    dat = unique(strat2[c("NTOT","YEAR")])
-    f = formula(NTOT ~ YEAR)
-    by = "YEAR"
-  }
-  xx = aggregate(f, data = dat, FUN = sum)
-  strat2$id = 1:nrow(strat2) #save order of strat2
-  xx = merge(strat2,xx, by = by)
-  xx = xx[order(xx$id),] ## Retrieve order of of strat2
-  strat2$wh = xx$NTOT.x/xx$NTOT.y
-  ## Clean Up
-  rm(xx)
-  strat2 = strat2[names(strat2) %w/o% "id"]
+  fn <- with(strat2, n/NTOT);
+  fm <- with(strat2, mbar/MTOT);
+  strat2$vbar <- with(strat2, ((1-fn)*v1/n)+((fn*(1-fm)*v2)/nm));
+  strat2$NMTOT <- with(strat2, NTOT*MTOT);
   
 return(strat2)
 }
