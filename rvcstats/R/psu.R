56dcbd9958672e43ac8f8aff8108985638241a17
Harry Ganz <hganz@rsmas.miami.edu>
2014-11-18 08:55:04 -0500

    Modified psu to account for changes in rvcData and ssu methods.

--- rvcstats/R/psu.R
+++ rvcstats/R/psu.R
@@ -1,43 +1,35 @@
-## Returns: the avg. counts or occurence (yi), 
+## Returns: a data.frame of the avg. counts or occurence (yi), 
 ## variance in counts or occurrence (vari), 
 ## number of SSUs (m), and replicate status (np.freq) for each PSU.
-## Given an RVC object
-.psu = function(rvcObj, calculate.density = TRUE){
-  ## Check type
-  if (!inherits(rvcObj, "RVC")){
-    stop("rvcObj must be of class RVC. Type ?rvcData for more information")
-  }
-  
-  ## Convert rvc obj to list
-  class(rvcObj) = "list"
-  
+## Given an RVC object and calc, a code for whether density or occurrence
+## should be calculated
+.psu = function(rvcObj, calc = "d"){
+  ## get the ssu data
+  ssu  <- .ssu(rvcObj, calc);
   ## Set the variables by which to aggregate
-  agg.by = rvcObj[names(rvcObj) %w/o% c("STATION_NR", "NUM")]
-  
-  ## Number of SSUs per PSU
-  psu= aggregate(rvcObj$STATION_NR, by = agg.by, FUN = length)# these two functions
-  ## should produce the same results, but they don't
-  names(psu)[length(names(psu))] = "m"
-  
-  ## Change to discrete dist. if calculate.density is FALSE
-  if (!calculate.density){
-    rvcObj$NUM = ifelse(rvcObj$NUM > 0, 1, 0)
+  agg_by  <- c("SPECIES_CD", "YEAR", "STRAT", "PRIMARY_SAMPLE_UNIT");
+  ## If includes_ptotected is TRUE add to agg_by vars
+  if (attr(rvcObj, "includes_protected")){
+    agg_by  <- c(agg_by, "PROT");
   }
+  ## Number of SSUs per PSU
+  psu  <- with(
+    ssu,
+    aggregate(list(m = STATION_NR), by = as.list(ssu[agg_by]), FUN = length)
+    );
 
   ## Avg. counts/occurence in SSU by PSU
-  psu$yi = aggregate(rvcObj$NUM, by = agg.by, FUN = mean)$x
+  psu$yi = aggregate(ssu$yi, by = as.list(ssu[agg_by]), FUN = mean)$x
   
-  ## If calculate density is TRUE calculate variance for 
+  ## If calc == 'd' calculate variance for 
   ## continuous distribution, else for binomial distribution
-  if (calculate.density){
+  if (calc == "d"){
     ## Variance in counts of each SSU by PSU
-    psu$vari = aggregate(rvcObj$NUM, by = agg.by,
-                         FUN = function(x){
-                           ifelse(is.na(var(x)),0,var(x))
-                         })$x
+    psu$vari = aggregate(ssu$yi, by = as.list(ssu[agg_by]),
+                         FUN = function(x){ifelse(is.na(var(x)),0,var(x))})$x;
     
   } else {
-    psu$vari = ifelse(psu$m==1, 0, psu$m/(psu$m-1)*psu$yi*(1-psu$yi))
+    psu$vari = ifelse(psu$m==1, 0, psu$m/(psu$m-1)*psu$yi*(1-psu$yi));
   }
   
   ## Replicate status
