a971ecd141a58ee8914eedd15459b8d1f4c2a789
Harry Ganz <hganz@rsmas.miami.edu>
2014-11-18 09:46:31 -0500

    Modified domain to relflect changes in strat

--- rvcstats/R/domain.R
+++ rvcstats/R/domain.R
@@ -1,22 +1,26 @@
-## Returns: The average density/occurence (yi) per SSU (177m^2), variance, SE, and CV
+## Returns: A data.frame of  
+## The average density/occurence (yi) per SSU (177m^2), variance, SE, and CV
 ## and total number of samples nm for the entire sampling domain
-## Given: An RVC object, a STRAT object, and optional parameters passed
-## to stratDensity
-domain = function(rvcObj, stratObj, calculate.density = TRUE, ...){
-  strat = strat(rvcObj, stratObj, calculate.density, ...)
+## Given: An RVC object, and calc, a character 'd' for density (default)
+## or 'p' for occurrence
+domain  <-  function(rvcObj, calc = "d"){
+  strat  <-  strat(rvcObj, calc)
   
   ## Select aggregate by variables
-  agg.by = as.list(strat[names(strat) %w/o% c("NTOT","STRAT", "wh", "yi", "mbar", "n",
-                                      "v1", "v2", "nm", "v2", "vbar", "NMTOT")])
-  ## Calculate density, variance, SE, CV, and nm
-  domain = with(strat, aggregate(wh*yi, by = agg.by, FUN = sum))
-  names(domain)[length(names(domain))] = "yi"
-  domain$variance = with(strat, aggregate(wh^2*vbar, by = agg.by, FUN = sum)$x)
+  agg_by  <- c("SPECIES_CD", "YEAR");
+  ## If includes_protected is TRUE, add to agg_by vars
+  if (attr(rvcObj, "includes_protected")){
+    agg_by  <- c(agg_by, "PROT")
+  }
+  agg_by = as.list(strat[agg_by])
+  ## Calculate density/occurrence, variance, SE, CV, and nm
+  domain = with(strat, aggregate(list(yi = wh*yi), by = agg_by, FUN = sum))
+  domain$variance = with(strat, aggregate(wh^2*vbar, by = agg_by, FUN = sum)$x)
   domain$se = sqrt(domain$variance)
   domain$cv = with(domain, (se/yi)*100)
-  domain$n = with(strat, aggregate(n, by = agg.by, FUN = sum)$x)
-  domain$nm = with(strat, aggregate(nm, by = agg.by, FUN = sum)$x)
-  domain$NMTOT = with(strat, aggregate(NMTOT, by = agg.by, FUN = sum)$x)
+  domain$n = with(strat, aggregate(n, by = agg_by, FUN = sum)$x)
+  domain$nm = with(strat, aggregate(nm, by = agg_by, FUN = sum)$x)
+  domain$NMTOT = with(strat, aggregate(NMTOT, by = agg_by, FUN = sum)$x)
   
   return(domain)
 }
