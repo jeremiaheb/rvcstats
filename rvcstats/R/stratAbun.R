42d3bc8761c4240c93a40b43f8d1563b746ab379
Harry Ganz <hganz@rsmas.miami.edu>
2014-12-04 10:41:04 -0500

    Modified stratAbun par name (r -> rvcObj) Modified par name to conform to rest of package, also changes value field in doc slightly

--- rvcstats/R/stratAbun.R
+++ rvcstats/R/stratAbun.R
@@ -8,13 +8,14 @@
 #' Indices of abundance produced by this function assume 
 #' homogeneity of density within strata, and should not be 
 #' used naively for assessment.
-#' @return A data.frame with 
+#' @return A data.frame with year, species, stratum, protected 
+#' status (if applicable) and:
 #' \item{wh}{The weighting factor for a particular stratum}
 #'  \item{yt}{The average abundance in each stratum}
 #'  \item{vbar}{Variance in mean abundance in each stratum}
 #'  @seealso \code{\link{rvcData}} \code{\link{strat}} \code{\link{domainAbun}}
-stratAbun  <- function(r){
-  s  <- strat(r, calc = "d")
+stratAbun  <- function(rvcObj){
+  s  <- strat(rvcObj, calc = "d")
   out  <- s[names(s) %in% c("SPECIES_CD", "YEAR", "PROT", "STRAT")];
   out$yt  <- with(s, round(yi*NMTOT,0));
   out$vbar  <- with(s, NMTOT^2*vbar);
