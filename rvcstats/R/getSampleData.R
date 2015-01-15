b5e7dd689cef5a5416e78a52825c9e33cfa1f7c6
Harry Ganz <hganz@rsmas.miami.edu>
2015-01-15 06:49:06 -0500

    Added getSampleData function
    Retrieves data from server

---
+++ rvcstats/R/getSampleData.R
@@ -0,0 +1,19 @@
+## Returns: A data frame of the sample data given a set of parameters
+## species, year, region, stratum, protected, and when_present
+.getSampleData  <- function(species, year = NULL, region = NULL, stratum = NULL,
+                           protected = NULL, when_present = NULL, 
+                           server = 'http://127.0.0.1:3000'){
+  require(RCurl);
+  require(RJSONIO);
+  # Put together URL and request
+  url  <- paste(server, '/api/samples.json', 
+          .toQuery(species = species, year = year, region = region,
+                   stratum = stratum, protected = protected, 
+                   when_present = when_present),
+          sep='');
+  # Get data and convert JSON to list
+  j  <- fromJSON(getURL(url));
+  # Turn list into data.frame 
+  out  <- do.call(rbind, lapply(j, as.data.frame));
+  return(out)
+}
