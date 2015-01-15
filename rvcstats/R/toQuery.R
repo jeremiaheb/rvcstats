4450815a0ed5f2f88693bd45a00af149a60bb794
Harry Ganz <hganz@rsmas.miami.edu>
2015-01-15 06:26:15 -0500

    Removed condition that didn't do anything

--- rvcstats/R/toQuery.R
+++ rvcstats/R/toQuery.R
@@ -4,21 +4,19 @@
 .toQuery <- function(...){
   # Get the parameters as a list
   dots  <- list(...);
-  if (length(dots) != 0){
-    # Get the names of the parameters
-    n = names(dots);
-    # Make each parameter value pair into a string of the form
-    # par[]=value
-    # TODO replace this with an apply function
-    p  <- NULL;
-    for (i in seq_along(dots)){
-      for (j in seq_along(dots[[i]])){
-        p = c(p, paste(n[i], dots[[i]][j], sep='[]='))
-      }
+  # Get the names of the parameters
+  n = names(dots);
+  # Make each parameter value pair into a string of the form
+  # par[]=value
+  # TODO replace this with an apply function
+  p  <- NULL;
+  for (i in seq_along(dots)){
+    for (j in seq_along(dots[[i]])){
+      p = c(p, paste(n[i], dots[[i]][j], sep='[]='))
     }
-    #Put all the parameter value pairs into one string 
-    # seperated by ampersands, and replace all spaces with plus signs
-    out <- gsub(' ','+',paste('?',paste(p, collapse='&'), sep=''))
-    return(out)
   }
+  #Put all the parameter value pairs into one string 
+  # seperated by ampersands, and replace all spaces with plus signs
+  out <- gsub(' ','+',paste('?',paste(p, collapse='&'), sep=''))
+  return(out)
 }
