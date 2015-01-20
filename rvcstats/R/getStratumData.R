6e6b94756c7e030427cf39b633197c229d7b892a
Harry Ganz <hganz@rsmas.miami.edu>
2015-01-20 11:05:30 -0500

    Added getStratumData function

---
+++ rvcstats/R/getStratumData.R
@@ -0,0 +1,15 @@
+.getStratumData  <- function(year, region, stratum,
+                               protected, server) {
+  require(RCurl);
+  require(RJSONIO);
+  # Put together URL and request
+  url  <- paste(server, '/api/strats.json', 
+                .toQuery(year = year, region = region,
+                         strat = stratum, prot = protected),
+                sep='');
+  # Get data and convert JSON to list
+  j  <- fromJSON(getURL(url));
+  # Turn list into data.frame 
+  out  <- do.call(rbind, lapply(j, as.data.frame));
+  return(out)
+}
