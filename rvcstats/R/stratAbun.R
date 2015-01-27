8ff775d61e3ef6c50f418d34bd27143e109d6634
Harry Ganz <hganz@rsmas.miami.edu>
2015-01-27 13:28:29 -0500

    Chnaged calc parameter to stat

--- rvcstats/R/stratAbun.R
+++ rvcstats/R/stratAbun.R
@@ -1,5 +1,5 @@
 stratAbun  <- function(rvcObj){
-  s  <- strat(rvcObj, calc = "d")
+  s  <- strat(rvcObj, stat = "density")
   out  <- s[names(s) %in% c("SPECIES_CD", "YEAR", "PROT", "STRAT")];
   out$yt  <- with(s, round(yi*NMTOT,0));
   out$vbar  <- with(s, NMTOT^2*vbar);
