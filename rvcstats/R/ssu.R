b9013bc24b04c1ac24018238db780f1f1d9cc5fe
Harry Ganz <hganz@rsmas.miami.edu>
2015-01-30 13:33:18 -0500

    renamed ssu_abundance to ssu

--- rvcstats/R/ssu.R
+++ rvcstats/R/ssu.R
@@ -1,33 +1,17 @@
-## Returns the density or percent occurrence for the secondary sampling
-## unit as a data.frame, given an RVC object and what should be 
-## calculated
-.ssu  <- function(rvcObj, stat="density"){
-  ## Check type
-  if (!inherits(rvcObj, "RVC")){
-    stop("rvcObj must be of class RVC. Type ?rvcData for more information")
+#' @export
+ssu  <- function(x, stat, merge_protected, growth_parameters){
+  # Convernt NUM to biomass if stat is biomass
+  if (stat == "biomass"){
+    x  <- toBiomass(x, growth_parameters);
   }
-  if (stat != "occurrence" & stat != "density" ){
-    stop("stat must be either 'occurrence' to for occurrence or 'density' for density")
+  # Covert NUM to occurrence if stat is occurrence
+  if (stat == "occurrence"){
+    x  <- toOccurrence(x);
   }
-  ## Set the variables by which to aggregate
-  agg_by  <- c("SPECIES_CD", "REGION", "YEAR", "STRAT", "PRIMARY_SAMPLE_UNIT",
-               "STATION_NR");
-  ## If merge_protected is false, add to agg_by vars
-  if (!attr(rvcObj, "merge_protected")){
-    agg_by  <- c(agg_by, "PROT");
-  }
-  ## If stat is "occurrence" change the function to be applied to aggregate
-  ## to produce discrete variable
-  if(stat == "occurrence"){
-    f  <- function(x){ifelse(sum(x)>0,1,0)};
-  } else {
-    f  <- sum;
-  }
-  ## Aggregate and return data
-  out  <- with(
-    rvcObj,
-    aggregate(list(yi = sample_data$NUM), by = as.list(sample_data[agg_by]),
-              FUN = f)
-    );
-  return(out)
+  # Calculate statistic at the SSU level
+  x$sample_data  <- with(x$sample_data, 
+                         aggregate(list(yi = NUM), aggBy("ssu",stat), sum)
+                         );
+  class(x)  <- "SSU";
+  return(x)
 }
