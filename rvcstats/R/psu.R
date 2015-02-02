859e0e7edd0e09fe6082884a28d65980164514f5
Harry Ganz <hganz@rsmas.miami.edu>
2015-02-02 14:16:52 -0500

    Modified comments
    Comments in function now make it clear where the arguments, cases, and evaluation sections are

--- rvcstats/R/psu.R
+++ rvcstats/R/psu.R
@@ -9,7 +9,7 @@ psu  <- function(x, stat, growth_parameters){
   ## Run ssu on x
   x  <- ssu(x, stat, growth_parameters);
   
-  ## Set up the arguments to be used in aggregate function
+  ## Arguments:
   # For summary statistic:
   args1  <- alist(x = list(yi = yi), by = aggBy("psu", stat), FUN = NULL);
   # For variance:
@@ -17,38 +17,36 @@ psu  <- function(x, stat, growth_parameters){
   # For m:
   args3  <- alist(x = list(m = STATION_NR), by = aggBy("psu", stat), FUN = NULL);
   
-  ## Cases for summary statistic
+  ## Cases:
+  # For statistic
   if (stat == "density" | stat == "occurrence"){
     args1$FUN  <- mean;
   } else {
     args1$FUN  <- sum;
   }
-  ## Cases for variance
+  # For variance:
   if (stat == "occurrence"){
-    # Discrete variance
-    pvar  <- function(x){mean(x)*(1-mean(x))}
-    args2$FUN  <-  function(x){ifelse(length(x)>1,length(x)/(length(x)-1)*pvar(x), NA)}
-    # Produces NAs if m == 1
+    pvar  <- function(x){mean(x)*(1-mean(x))}; # Discrete variance
+    args2$FUN  <-  function(x){ifelse(length(x)>1,length(x)/(length(x)-1)*pvar(x), NA)} # Produces NAs if m == 1
   } else {
-    args2$FUN  <- var;
-    ## Produces NAs if m == 1
+    args2$FUN  <- var; ## Produces NAs if m == 1
   }
-  ## All cases for m use the same function
+  # For m (all the same)
   args3$FUN  <- length;
   
-  ## Clone x$stratum_data into a list called out
+  ## Evaluation: 
+  # Clone x$stratum_data into a list called out
   out  <- list();
   out$stratum_data  <- x$stratum_data;
-  
-  ## If summary statistic is length_frequency, only calculate statistic value
-  ## else calculate value, variance, and m
+  # If summary statistic is length_frequency, only calculate statistic value
+  # else calculate value, variance, and m
   out$sample_data  <- with(x$sample_data, do.call(aggregate, args1));
   if (stat!="length_frequency"){
     out$sample_data$var  <- with(x$sample_data, do.call(aggregate, args2)$var);
     out$sample_data$m  <- with(x$sample_data, do.call(aggregate, args3)$m);
   }
   
-  ## Set class to PSU
+  ## Set class to PSU and return
   class(out)  <- "PSU"
   return(out)
 }
--- rvcstats/R/ssu.R
+++ rvcstats/R/ssu.R
@@ -9,27 +9,24 @@ ssu  <- function(x, stat, growth_parameters){
   if (!inherits(x, "RVC")){
     stop("x must be of type RVC")
   }
-  ## Arguments to use for each stat
-  # alist ensures arguments not evaluated until needed (lazy evaluation)
-  # General Case:
+  ## Arguments:
   args  <- alist(x = list(yi = NUM), by = aggBy("ssu",stat),FUN = NULL);
-  # For each stat:
+  
+  ## Cases:
   if (stat == "biomass"){
     # Set NUM to Biomass for each record
     x  <- toBiomass(x, growth_parameters);
-    # Sum biomass for each SSU
     args$FUN  <- sum;
   } else if(stat=="occurrence"){
-    # If occurrence, get occurrence for each SSU
-    args$FUN  <- function(x)ifelse(sum(x)>0,1,0);
+    args$FUN  <- function(x)ifelse(sum(x)>0,1,0); #Calculates occurrence
   } else {
-    # For density, abundance, and length_frequency
-    # Use sum as the function to apply
     args$FUN  <- sum;
   }
  
-  # Calculate statistic at the SSU level
+  ## Evaluation:
   x$sample_data  <- with(x$sample_data, do.call(aggregate, args));
+  
+  ## Set class to SSU and return
   class(x)  <- "SSU"
   return(x)
 }
