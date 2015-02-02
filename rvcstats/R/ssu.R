a7f91127594d499ad68be0026863a14a5e498e39
Harry Ganz <hganz@rsmas.miami.edu>
2015-02-02 13:41:02 -0500

    Modularized ssu cases:
    List of arguments to be passed to aggregate now at beggining of function
    
    Cases that modify these arguments in the middle
    
    Evaluation at end of function

--- rvcstats/R/ssu.R
+++ rvcstats/R/ssu.R
@@ -1,24 +1,35 @@
 #' @export
-ssu  <- function(x, stat, merge_protected, growth_parameters){
-  # Make sure x is an RVC object
+## Returns: the sample data as SSU level statistics given 
+## x, an RVC object
+## stat, the statistic to be calculated
+## growth_parameters, a list of allometric growth parameters (if
+## stat == "biomass")
+ssu  <- function(x, stat, growth_parameters){
+  ## Make sure x is an RVC object
   if (!inherits(x, "RVC")){
     stop("x must be of type RVC")
   }
-  # Recalculate NUM (if neccessary) and pick the function
-  # to use in the aggregate statement for each stat
+  ## Arguments to use for each stat
+  # alist ensures arguments not evaluated until needed (lazy evaluation)
+  # General Case:
+  args  <- alist(x = list(yi = NUM), by = aggBy("ssu",stat),FUN = NULL);
+  # For each stat:
   if (stat == "biomass"){
-    x  <- toBiomass(x, growth_parameters); #Turn num to biomass
-    f  <- sum; 
-  } else if (stat == "occurrence") {
-    f  <- function(x)ifelse(sum(x)>0,1,0); #Calc. occurrence for each SSU
+    # Set NUM to Biomass for each record
+    x  <- toBiomass(x, growth_parameters);
+    # Sum biomass for each SSU
+    args$FUN  <- sum;
+  } else if(stat=="occurrence"){
+    # If occurrence, get occurrence for each SSU
+    args$FUN  <- function(x)ifelse(sum(x)>0,1,0);
   } else {
-    f  <- sum; 
+    # For density, abundance, and length_frequency
+    # Use sum as the function to apply
+    args$FUN  <- sum;
   }
+ 
   # Calculate statistic at the SSU level
-  x$sample_data  <- with(x$sample_data, 
-                         aggregate(list(yi = NUM), aggBy("ssu",stat, merge_protected), f)
-                         );
-  # Set class to SSU
-  class(x)  <- "SSU";
+  x$sample_data  <- with(x$sample_data, do.call(aggregate, args));
+  class(x)  <- "SSU"
   return(x)
 }
