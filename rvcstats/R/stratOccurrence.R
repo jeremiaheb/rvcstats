5e9252b6df2fbeb9fb0c5d8ec96ab73fd4c83520
Harry Ganz <hganz@rsmas.miami.edu>
2015-02-04 14:25:38 -0500

    Added stratOccurrence function

---
+++ rvcstats/R/stratOccurrence.R
@@ -0,0 +1,10 @@
+#' @export
+## Returns: Stratum level occurrence given,
+## x, a psu object, and
+## merge_protected, a boolean indicating whether protected
+## and unprotected areas should be merged together
+stratOccurrence  <- function(x, merge_protected){
+  ## Basically just a wrapper for stratDensity
+  ## Only here in case someone wants to expand upon it
+  return(stratDensity(x, merge_protected));
+}
