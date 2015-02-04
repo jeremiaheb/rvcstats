e46328c0cca78860488d2955d3dbedc564e53bb0
Harry Ganz <hganz@rsmas.miami.edu>
2015-02-04 14:33:19 -0500

    Added stratBiomass function

---
+++ rvcstats/R/stratBiomass.R
@@ -0,0 +1,10 @@
+#' @export
+## Returns: Stratum level abundance given,
+## x, a psu object, and
+## merge_protected, a boolean indicating whether protected
+## and unprotected areas should be merged together
+stratBiomass  <- function(x, merge_protected){
+  ## Basically just a wrapper for stratAbundance
+  ## Only here in case someone wants to expand upon it
+  return(stratAbundance(x, merge_protected))
+}
