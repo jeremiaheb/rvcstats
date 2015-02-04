6a670d4dd2ee8c94db0aed31045053120e7f17da
Harry Ganz <hganz@rsmas.miami.edu>
2015-02-04 13:26:16 -0500

    Changed local variable name from x to out

--- rvcstats/R/strat.R
+++ rvcstats/R/strat.R
@@ -3,13 +3,13 @@ strat  <- function(x, stat, growth_parameters, merge_protected){
   # Get PSU data
   x  <- psu(x, stat, growth_parameters);
   # Set up cases
-  x  <- switch(
+  out  <- switch(
     stat,
     density = stratDensity(x, merge_protected),
     occurrence = stratOccurrence(x, merge_protected),
     abundance = stratAbundance(x, merge_protected),
     length_frequency = stratLengthFrequency(x, merge_protected),
     biomass = stratBiomass(x, merge_protected)
-    )
-  return(x)
+    );
+  return(out)
 }
