8cfaee6c54843a4f8e3b871e5fae6cd27cfd3494
Harry Ganz <hganz@rsmas.miami.edu>
2015-02-04 14:11:36 -0500

    Added stratAbundance function
    Removed old stratAbun function
    Added new stratAbundance function

--- rvcstats/R/stratAbun.R
+++
@@ -1,7 +0,0 @@
-stratAbun  <- function(rvcObj){
-  s  <- strat(rvcObj, stat = "density")
-  out  <- s[names(s) %in% c("SPECIES_CD", "YEAR", "PROT", "STRAT")];
-  out$yt  <- with(s, round(yi*NMTOT,0));
-  out$vbar  <- with(s, NMTOT^2*vbar);
-  return(out)
-}
---
+++ rvcstats/R/stratAbundance.R
@@ -0,0 +1,11 @@
+#' @export
+## Returns: Stratum level abundance given,
+## x, a psu object, and
+## merge_protected, a boolean indicating whether protected
+## and unprotected areas should be merged together
+stratAbundance  <- function(x, merge_protected){
+  out  <- stratDensity(x, merge_protected);
+  out$yi  <- with(out, round(yi*NMTOT,0));
+  out$var  <- with(out, NMTOT^2*var);
+  return(out)
+}
