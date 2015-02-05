d64217c3de794535c18f0084af6464b103bf873c
Harry Ganz <hganz@rsmas.miami.edu>
2015-02-05 16:16:35 -0500

    Added comments to strat function

--- rvcstats/R/strat.R
+++ rvcstats/R/strat.R
@@ -1,4 +1,11 @@
 #' @export
+## Returns domain-level summary statistics given,
+## x, and rvc object
+## stat, the requested statistics
+## growth_parameters, a list of the allometric growth parameters
+## a and b, if biomass is selected
+## merge_protected, a boolean indicated whether protected
+## and unprotected areas should be merged together
 strat  <- function(x, stat, growth_parameters, merge_protected){
   # Get PSU data
   x  <- psu(x, stat, growth_parameters);
