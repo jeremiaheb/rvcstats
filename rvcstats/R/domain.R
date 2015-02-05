9c706813243c53c8d048754e1371fb1c24074141
Harry Ganz <hganz@rsmas.miami.edu>
2015-02-05 16:17:52 -0500

    Domain function now wraps a set of functions
    Must functionality of domain function transferred to domainDensity function

--- rvcstats/R/domain.R
+++ rvcstats/R/domain.R
@@ -1,36 +1,23 @@
-#' Domain level estimates of density/occurrence
-#' @export
-#' @description Outputs the domain level estimates of density or 
-#' occurrence given an RVC object
-#' @inheritParams strat
-#' @return A data.frame with the species, year, and stratum 
-#' information as well as:
-#' \item{yi}{The average density/occurrence}
-#' \item{variance}{The variance in density/occurrence}
-#' \item{se}{The standard error of density/occurrence}
-#' \item{cv}{The coeficient of variation (as a percent) in density/occurrence}
-#' \item{n}{The total number of primary samples per sampling domain}
-#' \item{nm}{The total number of secondary samples per sampling domain}
-#' \item{NMTOT}{The total possible number of secondary samples per sampling domain}
-#' @seealso \code{\link{rvcData}} \code{\link{strat}}
-domain  <-  function(rvcObj, stat = "density"){
-  strat  <-  strat(rvcObj, stat)
-  
-  ## Select aggregate by variables
-  agg_by  <- c("SPECIES_CD", "YEAR");
-  ## If merge_protected is FALSE, add to agg_by vars
-  if (!attr(rvcObj, "merge_protected")){
-    agg_by  <- c(agg_by, "PROT")
-  }
-  agg_by = as.list(strat[agg_by])
-  ## Calculate density/occurrence, variance, SE, CV, and nm
-  domain = with(strat, aggregate(list(yi = wh*yi), by = agg_by, FUN = sum))
-  domain$variance = with(strat, aggregate(wh^2*vbar, by = agg_by, FUN = sum)$x)
-  domain$se = sqrt(domain$variance)
-  domain$cv = with(domain, (se/yi)*100)
-  domain$n = with(strat, aggregate(n, by = agg_by, FUN = sum)$x)
-  domain$nm = with(strat, aggregate(nm, by = agg_by, FUN = sum)$x)
-  domain$NMTOT = with(strat, aggregate(NMTOT, by = agg_by, FUN = sum)$x)
-  
-  return(domain)
+#'@export
+## Returns domain-level summary statistics given,
+## x, and rvc object
+## stat, the requested statistics
+## growth_parameters, a list of the allometric growth parameters
+## a and b, if biomass is selected
+## merge_protected, a boolean indicated whether protected
+## and unprotected areas should be merged together
+## when_present, a boolean indicating whether statistic
+## is to be calculated only when species is present
+domain  <- function(x, stat, growth_parameters, merge_protected, when_present){
+  x  <- strat(x, stat, growth_parameters, merge_protected);
+  x <- addWeighting(x, merge_protected, when_present);
+  out  <- switch(
+    stat,
+    abundance = domainAbundance(x, merge_protected),
+    density = domainDensity(x, merge_protected),
+    occurrence = domainOccurrence(x, merge_protected),
+    length_frequency = domainLengthFrequency(x, merge_protected),
+    biomass = domainAbundance(x, merge_protected)
+    );
+  return(out)
 }
