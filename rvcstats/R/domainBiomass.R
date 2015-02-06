d69a83eee75fee61c31b7412bb99116e66fc5e9d
Harry Ganz <hganz@rsmas.miami.edu>
2015-02-06 11:23:51 -0500

    Added domainBiomass function

---
+++ rvcstats/R/domainBiomass.R
@@ -0,0 +1,9 @@
+#' @export
+## Returns: Domain level biomass given,
+## x, a data.frame of strat data, and
+## merge_protected, a boolean indicating whether protected
+## and unprotected areas should be merged together
+domainBiomass  <- function(x, merge_protected){
+  ## A wrapper for abundance
+  return(domainAbundance(x, merge_protected))
+}
