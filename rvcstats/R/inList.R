573b916aab0cc58bec92d415710d5f62baeaf882
Harry Ganz <hganz@rsmas.miami.edu>
2015-02-06 12:29:19 -0500

    Removed deprecated functions

--- rvcstats/R/inList.R
+++
@@ -1,8 +0,0 @@
-## Helper function to produce errors if parameter par composed of x is not in list y
-.inList = function(par,x,y){
-  d = setdiff(x,y)
-  if (length(d)>0){
-    diff = paste(d, collapse = ", ")
-    stop(paste(par,diff, "not found in data"))
-  }
-}
--- rvcstats/R/mStar.R
+++
@@ -1,9 +0,0 @@
-mStar  <- function(rvcObj){
-  ## Get stratum data
-  s  <- strat(rvcObj, calc = "d");
-  ## Calculate mstar
-  s$mstar  <- with(s, sqrt(v2)/s);
-  s$mstar[is.nan(s$mstar)]  <- 0;
-  ## Return
-  return(s)
-}
