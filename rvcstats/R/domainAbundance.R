3add4b4a8f26540daaaa8d700c0de8f8c46c842e
Harry Ganz <hganz@rsmas.miami.edu>
2015-02-06 09:45:57 -0500

    Added domainAbundance function

---
+++ rvcstats/R/domainAbundance.R
@@ -0,0 +1,14 @@
+#' @export
+## Returns Domain level abundance estimates given,
+## x, a data.frame of stratum-level estimates
+## merge_protected, a boolean indicating whether
+## protected and unportected areas should be merged together
+domainAbundance  <- function(x, merge_protected){
+  ## Arguments
+  args1  <- alist(x = list(NTOT = NTOT, NMTOT = NMTOT, yi = yi, var = var, n = n, nm = nm),
+                  aggBy("domain", "density", merge_protected),
+                  FUN = sum);
+  ## Evaluation
+  out  <- with(x,do.call(aggregate, args1));
+  return(out)
+}
