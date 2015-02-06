a67e4c16e0a803454fbc97a657ff047163dbe304
Harry Ganz <hganz@rsmas.miami.edu>
2015-02-06 11:37:49 -0500

    Added na.rm = T to variance
    NAs ignored when handling weighted variance. Useful for when_present = TRUE and some strata may not have had two SSUs at a PSU where a species was present.

--- rvcstats/R/domainDensity.R
+++ rvcstats/R/domainDensity.R
@@ -9,17 +9,15 @@ domainDensity  <- function(x, merge_protected){
                   FUN = sum);
   args2  <- alist(x = list(NMTOT = NMTOT), aggBy("domain", "density", merge_protected),
                   FUN = sum);
-  args3  <- alist(x = list(yi = weighted_yi), aggBy("domain", "density", merge_protected),
+  args3  <- alist(x = list(yi = wh*yi), aggBy("domain", "density", merge_protected),
                   FUN = sum);
-  args4  <- alist(x = list(var = weighted_var), aggBy("domain", "density", merge_protected),
-                  FUN = sum);
+  args4  <- alist(x = list(var = wh^2*var), aggBy("domain", "density", merge_protected),
+                  FUN = sum, na.rm = TRUE);
   args5  <- alist(x = list(n = n), aggBy("domain", "density", merge_protected), 
                   FUN = sum);
   args6  <- alist(x = list(nm = nm), aggBy("domain", "density", merge_protected), 
                   FUN = sum);
   ## Evaluation
-  weighted_yi  <- with(x, wh*yi);
-  weighted_var  <- with(x, wh^2*var);
   out  <- with(x, do.call(aggregate, args1));
   out$NMTOT <- with(x, do.call(aggregate, args2)$NMTOT);
   out$yi <- with(x, do.call(aggregate, args3)$yi);
