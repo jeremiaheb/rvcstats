b327d5bb5acc120a229a7cfaa48cd4859ddeb6cc
Harry Ganz <hganz@rsmas.miami.edu>
2015-02-06 09:45:42 -0500

    Added domainOccurrence function

---
+++ rvcstats/R/domainOccurrence.R
@@ -0,0 +1,9 @@
+#' @export
+## Returns: Domain level density given,
+## x, a data.frame of strat data, and
+## merge_protected, a boolean indicating whether protected
+## and unprotected areas should be merged together
+domainOccurrence  <- function(x, merge_protected){
+  ## Just a wrapper for density
+  return(domainDensity(x, merge_protected))
+}
