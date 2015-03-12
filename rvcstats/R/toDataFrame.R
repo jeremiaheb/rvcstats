412152c1cd95b4f157b4479df71d05e2e1849ca4
Harry Ganz <hganz@rsmas.miami.edu>
2015-03-12 15:24:32 -0400

    added toDataFrame method
    new method for converting to data.frames considerably faster than previous method

---
+++ rvcstats/R/toDataFrame.R
@@ -0,0 +1,12 @@
+toDataFrame  <- function(l){
+  ## Figure out which elements are numeric
+  isNum  <- sapply(l[[1]], is.numeric);
+  ## Cbind and Rbind everything
+  df  <-  do.call(rbind,lapply(l, function(x){do.call(cbind,x)}));
+  ## Cast to data.frame
+  df  <- as.data.frame(df);
+  ## Cast numeric cols to numeric
+  df[isNum]  <- apply(df[isNum],2,as.numeric);
+  
+  return(df)
+}
