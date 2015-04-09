13618e7879a4e77d4c0d52987a6e8c7186f9a532
Harry Ganz <hganz@rsmas.miami.edu>
2015-04-09 10:46:48 -0400

    Single species case for getStat
    Handles signle species case of getStat , can also handle multi-species case if length_class = NULL, stat!="biomass" or when_present is FALSE

---
+++ rvcstats/R/getStatSingle.R
@@ -0,0 +1,27 @@
+## Handles the single species case for getStat, can handle multi-species
+## if stat != biomass, length_class == NULL, or when_present == FALSE
+getStatSingle  <- function(x, level, stat, growth_parameters, merge_protected, when_present,
+                     length_class, ...){
+  # Handle ... argument with select function
+  # suppress warnings already handled in getStat main function
+  x  <- suppressWarnings(select(x, ...));
+  # if stat is biomass set growth parameters appropriately
+  if (stat == "biomass"){
+    growth_parameters  <- setGrowthParameters(x, growth_parameters);
+  }
+  # If when_present subset by NUM > 0
+  if (when_present){
+    # Subset by when_present
+    x$sample_data  <- subset(x$sample_data, NUM > 0);
+  }
+  # If length_class, add length_class and run once on each
+  # else run base case
+  if (!is.null(length_class)){
+    out  <- lengthClass(x, length_class, level, stat, growth_parameters,
+                merge_protected, when_present);
+  } else {
+    out  <- getStatBase(x, level, stat, growth_parameters,
+                     merge_protected, when_present)
+  }
+  return(out)
+}
