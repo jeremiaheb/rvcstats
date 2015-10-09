f7cba13b46df9958ea1c2d8d009ed5699cc6fffe
Harry Ganz <hganz@rsmas.miami.edu>
2015-10-09 11:03:55 -0400

    Prevent data from being simplified before cbind
    If all the data is "NA" R reads it as a character vector and will not cbind, to prevent this coerce it to list

--- rvcstats/R/toDataFrame.R
+++ rvcstats/R/toDataFrame.R
@@ -2,7 +2,7 @@ toDataFrame  <- function(l){
   ## Figure out which elements are numeric
   isNum  <- sapply(l[[1]], is.numeric);
   ## Cbind and Rbind everything
-  df  <-  do.call(rbind,lapply(l, function(x){do.call(cbind,x)}));
+  df  <-  do.call(rbind,lapply(l, function(x){do.call(cbind,as.list(x))}));
   ## Cast to data.frame
   df  <- as.data.frame(df);
   ## Cast numeric cols to numeric
